fff46bd58475cd49b68a67e3037eacff1eeb81a5
Benjamin <benjaminsalasek@Athena.local>
2018-07-14 23:42:16 -0500

    final answer successfully submitted

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+var myArray;
+var answer = 0;
+
+duplicateCount("aAaezbrcAderb"); //should be 
+
 function duplicateCount(text){
-var a;
+
+	text = text.toUpperCase();
+	myArray = text.split('');
+	myArray.sort();
+//	myArray.sort((a, b) => a.localeCompare(b, {ignorePunctuation: true}));
+//	console.log(myArray);
 	
-	
+	for(var i = 0; i < myArray.length; i++){
+
+		if(myArray[i] != myArray[i+1]){
+		}
+		else if(myArray[i] == myArray[i - 1]){
+		}
+		else{
+			answer++;
+		}
+		console.log(answer);
+	}
+	return answer;
 }
